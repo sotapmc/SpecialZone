2783ad952475d84aca49b03c55c5fa6dffc86b0c
Subilan <suyuanliang1995@gmail.com>
2020-04-14 23:23:50 +0800

    main v1

---
+++ src/main/java/org/sotap/SpecialZone/SpecialZone.java
@@ -0,0 +1,20 @@
+package org.sotap.SpecialZone;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SpecialZone extends JavaPlugin {
+    public CommandHandler executor;
+
+	@Override  
+    public void onEnable(){  	
+        this.saveDefaultConfig();
+        this.getLogger().info("SpecialZone has been enabled successfully.");
+        //getServer().getPluginManager().registerEvents(new Events(this), this); // register events
+        Bukkit.getPluginCommand("setspecialzone").setExecutor(new CommandHandler(this)); // register command
+    }  
+  
+    @Override      
+    public void onDisable(){
+        this.getLogger().info("SpecialZone has been disabled successfully.");
+    }
+}  
