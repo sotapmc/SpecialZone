87d849a6caf3ef3976fc0ee34bf1cbd257c9628d
Subilan <suyuanliang1995@gmail.com>
2020-04-21 10:38:01 +0800

    Internal state controller

---
+++ src/main/java/org/sotap/SpecialZone/States.java
@@ -0,0 +1,36 @@
+package org.sotap.SpecialZone;
+
+import java.util.List;
+
+import org.bukkit.Location;
+
+public class States {
+    public static String selectState = "";
+    public static List<Location> locations;
+
+    private Integer getIndexBySelection(String selectState) {
+        return selectState == "selection-1" ? 1 : (selectState == "selection-2" ? 2 : 0);
+    }
+
+    public void nextSelectState() {
+        String selectStatePrev = States.selectState;
+        selectState = selectStatePrev == "" ? "selection-1" : (selectStatePrev == "selection-1" ? "selection-2" : (selectStatePrev == "selection-2" ? "" : ""));
+    }
+
+    public void resetSelectState() {
+        selectState = "";
+        locations.clear();
+    }
+
+    public void storeLocation(String selectState, Location location) {
+        locations.set(this.getIndexBySelection(selectState), location);
+    }
+
+    public Location getLocation(String selectState) {
+        try {
+            return locations.get(this.getIndexBySelection(selectState));
+        } catch (IndexOutOfBoundsException e) {
+            return null;
+        }
+    }
+}
